97c2c07a60f0379d57395ce220f6dd4eb55f7f76
sunchenggao <sunchenggao.lbf@gmail.com>
2022-12-25 02:47:35 +0900

    multiple render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import "./styles.css";
+
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
-// import { Suspense } from "react";
-import Army from './component/Army'
-// import Sunlight from './component/Sunlight';
-import Lights from './component/Lights';
+import { Environment, OrbitControls, useFBX } from "@react-three/drei";
+import { Suspense } from "react";
 
+const Scene = () => {
+  const fbx = useFBX("Player.fbx");
+  return <primitive object={fbx} position={[-5,0,0]} scale={0.005} />;
+};
+const Scene2 = () => {
+  const fbx = useFBX("Player2.fbx");
 
-
+  return <primitive object={fbx} position={[5,0,0]} scale={0.005} />;
+};
 export default function App() {
   return (
     <div className="App">
       <Canvas>
-        <Lights />
-        <Army />
-        <OrbitControls />
-        <Environment preset="sunset" background />
+        <Suspense fallback={null}>
+          <Scene />
+          <Scene2 />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
       </Canvas>
     </div>
   );
--- src/component/Army.js
+++ src/component/Army.js
@@ -3,8 +3,6 @@ export default function Army(){
     return(
         <>
         <Soldier position={[-2,0,0]} />
-        <Soldier position={[2,0,0]} />
-        <Soldier position={[4,0,0]} />
         </>
     );
 }
